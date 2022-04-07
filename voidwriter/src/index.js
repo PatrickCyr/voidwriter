34191e76533436aa68d62adfe31e40305055e8a1
Patrick Cyr <p.bob.cyr@gmail.com>
2022-04-07 16:26:43 -0700

    Janky but working version.

--- voidwriter/src/index.js
+++ voidwriter/src/index.js
@@ -18,14 +18,15 @@ class Glyph extends React.Component {
   }
   
   updateCanvas() {
-    this.state.ctx = this.refs.canvas.getContext('2d');
+    let newState = {
+      ctx: this.refs.canvas.getContext('2d'),
+      images: [],
+      images_to_load: -1,
+    };
     
     let str = this.props.value;
     let chars = str.split("");
     let letter_main = chars[0];
-    let letter_mid = chars.length > 1 ? chars[1] : " ";
-    let letter_sup = chars.length > 2 ? chars[2] : " ";
-    let letter_sub = chars.length > 3 ? chars[3] : " ";
 
     const path = "images/";
     const suffix = ".png";
@@ -33,65 +34,86 @@ class Glyph extends React.Component {
     let imageCount = 1;
     let paths = [];
     paths[0] = path + letter_main + "main" + suffix;
-    if (chars.length > 1 && ' ' != chars[1]) {
-      paths[1] = path + letter_main + "mid" + chars[1] + suffix;
-      ++imageCount;
+    for (let ix = 1; ix < chars.length; ++ix) {
+      if (chars.length > ix && ' ' !== chars[ix]) {
+        paths[ix] = path + letter_main + ix + chars[ix] + suffix;
+        ++imageCount;
+      }
     }
-    if (chars.length > 2 && ' ' != chars[2]) {
-      paths[2] = path + letter_main + "sup" + chars[2] + suffix;
-      ++imageCount;
-    }
-    if (chars.length > 3 && ' ' != chars[3]) {
-      paths[3] = path + letter_main + "sub" + chars[3] + suffix;
-      ++imageCount;
-    }
-    this.state.images_to_load = imageCount;
-
-    let state = this.state;
+    newState.images_to_load = imageCount;
     
-    for (let i = 0; i < 4; ++i) {
+    for (let i = 0; i < chars.length; ++i) {
       if (paths[i]) {
         let newImg = new Image();
         newImg.src = paths[i];
-        this.state.images[i] = newImg;
+        newState.images[i] = newImg;
         newImg.onload = function() {
-          state.images_to_load -= 1;
-          if (state.images_to_load <= 0) {
-            for (let i = 0; i < 4; ++i) {
-              if (null != state.images[i]) {
-                state.ctx.drawImage(state.images[i], 0, 0);
+          // *** onload handler
+          newState.images_to_load -= 1;
+          if (newState.images_to_load <= 0) {
+            let w = -1;
+            let h = -1;
+            // Determine total width and height of all images.
+            for (let ii = 0; ii < chars.length; ++ii) {
+              if (null != newState.images[i]) {
+                w = Math.max(w, newState.images[i].width);
+                h = Math.max(h, newState.images[i].height);
+              }
+            }
+
+            // Resize the canvas.
+            newState.ctx.canvas.width = w;
+            newState.ctx.canvas.height = h;
+
+            // Draw images into canvas.
+            for (let jj = 0; jj < chars.length; ++jj) {
+              if (null != newState.images[jj]) {
+                newState.ctx.drawImage(newState.images[jj], 0, 0);
               }
             }
           }
+          // *** onload handler end
         }
       }
     }
+
+    this.setState(newState);
   }
   
   render() {
-    const CANVAS_WIDTH = 200;
-    const CANVAS_HEIGHT = 200;
-    
+    // These 1s are replaced with the real dimensions once all the images are loaded.
     return (
-      <canvas ref="canvas" width={300} height={300}> </canvas>
+      <canvas ref="canvas" width={1} height={1}> </canvas>
     );
   }
 }
 
+var nextGlyphKey = 0;
+
 class GlyphContainer extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  
   render() {
+
+    //let input = "o, oi, o v, oiv, o  d, oi d, o vd, oivd";
+    let input = this.props.value;
+    
+    let inputArray = input.split(',');
+    
+    let glyphArray = [];
+    for (let i = 0; i < inputArray.length; ++i) {
+      // Remove leading and trailing whitespace.
+      inputArray[i] = inputArray[i].trim();
+      
+      let key = "glyph_"+(nextGlyphKey++);
+      glyphArray[i] = <Glyph key={key} value={inputArray[i]} />
+    }
+    
     return (
       <div>
-        <hr />
-          <Glyph value="o" />
-          <Glyph value="oi"/>
-          <Glyph value="o v" />
-          <Glyph value="oiv"/>
-          <Glyph value="o  d" />
-          <Glyph value="oi d" />
-          <Glyph value="o vd" />
-          <Glyph value="oivd" />
-        <hr />
+        {glyphArray}
       </div>
     );
   }
@@ -99,15 +121,26 @@ class GlyphContainer extends React.Component {
 
 
 class Game extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {text: ""};
+
+    this.onTextChanged = this.onTextChanged.bind(this);
+  }
+  
+  onTextChanged(event) {
+    this.setState({text: event.target.value})
+  }
+  
   render() {
     return (
       <div className="game">
         <h1>VOiD Writer</h1>
-        <hr></hr>
-        Write text here:
-        <textarea id='input_textarea' defaultValue='o,oi,oiv,oivd'></textarea>
+        Write text here. Separate each glyph with a comma.
+        <textarea id='input_textarea' defaultValue='o,oi,oiv,oivd' onChange={this.onTextChanged}></textarea>
         <div className="glyph-container">
-          <GlyphContainer />
+          <GlyphContainer value={this.state.text}/>
         </div>
       </div>
     );
